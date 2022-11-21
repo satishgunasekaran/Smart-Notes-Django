b60b7b859891d107b8095c5e27411622a9d2d42f
Satish G <satishdevs@hotmail.com>
2022-11-21 17:54:53 +0530

    Added the login page

--- notes/views.py
+++ notes/views.py
@@ -1,25 +1,51 @@
 from urllib import request
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import ListView, DetailView, CreateView
+from django.http.response import HttpResponseRedirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Notes
 from .forms import NotesForm
 
-class NotesCreateView(CreateView):
+class NotesDeleteView(LoginRequiredMixin,DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name ="notes/notes_confirm_delete.html"
+    login_url ="/login"
+
+class NotesUpdateView(LoginRequiredMixin,UpdateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url ="/login"
 
-class NotesListView(ListView):
+class NotesCreateView(LoginRequiredMixin,CreateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+    login_url ="/login"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class NotesListView(LoginRequiredMixin,ListView):
     model = Notes
     context_object_name= "notes" 
     template_name = "notes/notes_list.html"
+    login_url ="/login"
 
-class NotesDetailView(DetailView):
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
+class NotesDetailView(LoginRequiredMixin,DetailView):
     model = Notes
     context_object_name = "note"
-
+    login_url ="/login"
 
 # def list(request):
 #     all_notes = Notes.objects.all()
